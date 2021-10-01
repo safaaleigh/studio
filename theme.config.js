adc443c39fcffa06cd2da6c77253ea1cf1f26c61
Safaa Hansen <safaalhansen@icloud.com>
2021-10-01 00:41:48 -0700

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -1,6 +1,11 @@
 const YEAR = new Date().getFullYear()
 
 export default {
+  github: 'https://github.com/safaaleigh/studio',
+  docsRepositoryBase: 'https://github.com/safaaleigh/studio/blob/main',
+  search: true,
+  prevLinks: true,
+  nextLinks: true,
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Safaa Hansen.
